7672d12286df3993ecd113e4c925ada605876a54
MauriceGeijsen <mauricegeijsen@gmail.com>
2022-06-20 12:48:53 +0200

    Latest update

--- Simulations/uva_alpide/cry_v1.7/geant/src/RNGWrapper.cc
+++
@@ -1,6 +0,0 @@
-#include "RNGWrapper.hh"
-#include "Randomize.hh"
-
-template CLHEP::HepRandomEngine* RNGWrapper<CLHEP::HepRandomEngine>::m_obj;
-
-template double (CLHEP::HepRandomEngine::*RNGWrapper<CLHEP::HepRandomEngine>::m_func)(void);
--- Simulations/uva_alpide/cry_v1.7/setup.sh
+++
@@ -1,8 +0,0 @@
-#
-# Do not call this directly, use
-# 
-#   eval `./setup`
-#
-export CRYHOME=$1
-export CRYDATAPATH=$1/data
-
